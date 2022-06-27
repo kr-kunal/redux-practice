5aa69d7fa680b1ff814df243bbf6ec3479a07728
kr-kunal <kunal.p@knackroot.com>
2022-06-27 09:27:13 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Products from "../components/Products";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="heading">Products</h2>
+      <section>
+        <Products />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
